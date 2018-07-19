0aafd42df882248fc65845b490315fdeb1e2bbe1
willorko8926 <willianreyes52@hotmail.com>
2018-07-19 10:13:00 -0500

    xd

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <body>
 
         <?php
-            echo "¡Hola, soy un script de PHP!";
+            echo "Hola mi amor trabajo para ti, nuestra pagina web oficial";
+            echo "11-03-2018 ";
+
         ?>
 
     </body>
